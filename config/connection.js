69438ec87f1f83dcfd9e7ae9470b9d476921ab50
Vince216 <vinceshury@gmail.com>
2019-04-26 08:34:59 -0400

    added server code. removed connection.js ln.7

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "honda5363CWRU.",
+  password: "",
   database: "burgers_db"
 });
 // Make connection.
