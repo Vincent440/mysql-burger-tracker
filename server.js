2bfffebd3c25531480dd76d25918ad6446147352
Vince216 <vinceshury@gmail.com>
2019-04-30 14:41:53 -0400

    pushing up files filling in the code slowly

--- server.js
+++ server.js
@@ -22,7 +22,5 @@ var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+//"start" server
+app.listen(PORT,() => console.log(`Server listening on: http://localhost:${PORT}`));
