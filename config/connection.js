3db9ee58f542cf981da2cc3422913d9875752def
Vince216 <vinceshury@gmail.com>
2019-05-01 17:17:19 -0400

    adding connection settings for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,17 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-console */
-// Set up MySQL connection.
 const mysql = require("mysql");
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "PASSWORD",//
-  database: "burgers_db"
-});
-// Make connection.
-connection.connect((err)=> {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "PASSWORD",
+    database: "burgers_db"
+  });
+}
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
