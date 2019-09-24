4480fbe8637da17f02d99d8850643afe86995dfc
vince s <vinceshury@gmail.com>
2019-09-23 20:53:17 -0400

    updated npm packages, styling, js to es6, and rewrote ORM

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "PASSWORD",
-    database: "burgers_db"
+    host: process.env.HOST,
+    port: process.env.DB_PORT,
+    user: process.env.USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
   });
 }
 connection.connect();
--- config/orm.js
+++ config/orm.js
@@ -1,19 +1,19 @@
 const connection = require("./connection.js");
 
 function printQuestionMarks(num) {
-    var arr = [];
-    for (var i = 0; i < num; i++) {
+    let arr = [];
+    for (let i = 0; i < num; i++) {
       arr.push("?");
     }
     return arr.toString();
 }
 
 // Helper function to convert object key/value pairs to SQL syntax
-function objToSql(ob) {
-    var arr = [];// loop through the keys and push the key/value as a string int arr
-    for (var key in ob) {
-      var value = ob[key];// check to skip hidden properties
-      if (Object.hasOwnProperty.call(ob, key)) {// if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+function objectToSQL(object) {
+    let arr = [];// loop through the keys and push the key/value as a string int arr
+    for (let key in object) {
+      let value = object[key];// check to skip hidden properties
+      if (Object.hasOwnProperty.call(object, key)) {// if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
         if (typeof value === "string" && value.indexOf(" ") >= 0) {
           value = "'" + value + "'";
         }// e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
@@ -25,32 +25,27 @@ function objToSql(ob) {
 }
 
 const orm = {
-    all:(tableName,cb)=>{// selectAll
-        let queryString = "SELECT * FROM " + tableName + ";";
-        connection.query(queryString, (err, result)=> {
+    all:(tableName, callBack) => {
+        let queryString = `SELECT * FROM ${tableName};`;
+        connection.query(queryString, (err, result) => {
             if (err) throw err;
             
-            cb(result);
+            callBack(result);
         });
     },
     create:(tableName, cols, vals, cb )=>{ //insertOne
-        var queryString = "INSERT INTO " + tableName;
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";  
-        //console.log(queryString);
+        const queryString = `INSERT INTO ${tableName} ( ${cols.toString()})
+        VALUES (${printQuestionMarks(vals.length)});`;  
+        console.log(queryString);
         connection.query(queryString, vals, (err, result)=> {
             if (err) throw err;
             cb(result);
         });       
     },
     update: (tableName, objColVals, condition, cb)=> {
-        var queryString = "UPDATE " + tableName;
+        let queryString = "UPDATE " + tableName;
         queryString += " SET ";
-        queryString += objToSql(objColVals);
+        queryString += objectToSQL(objColVals);
         queryString += " WHERE ";
         queryString += condition;
         //console.log(queryString);
