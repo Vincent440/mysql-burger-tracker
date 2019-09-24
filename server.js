82d0bb74c6b3b0f6ff2d6d83cefd90795decf6f1
vince s <vinceshury@gmail.com>
2019-09-23 23:44:06 -0400

    removed comments and moved constants to top of file

--- server.js
+++ server.js
@@ -2,16 +2,12 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
+const expressHandlebars = require('express-handlebars');
+const routes = require('./routes');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Set Handlebars.
-const expressHandlebars = require('express-handlebars');
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-// Import routes and give the server access to them.
-const routes = require('./routes');
 app.use(routes);
-// Start our server so that it can begin listening to client requests.
-//'start' server
 app.listen(PORT, () => console.log(`Server listening on: http://localhost:${PORT}`));
